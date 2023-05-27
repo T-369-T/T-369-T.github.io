03f0c340cecfe1d8b7c768207c944f82ce28c096
T-369 <T.369.TT.369.T@GMail.COM>
2023-05-27 17:57:20 +0800

    Update

--- Scripts/Typing.js
+++ Scripts/Typing.js
@@ -1,4 +1,21 @@
-new TypeIt("#Typing", {
-  strings: ['Name = "T-369"'],
-  speed: 75,
-}).go();
+function Typing_Code() {
+  new TypeIt(".Code", {
+    speed: 100,
+    afterComplete: async () => {
+      $("#Typing").css({ "text-align": "left", margin: "auto" });
+      hljs.highlightAll();
+    },
+  }).go();
+}
+
+function Typing() {
+  new TypeIt(".Typing", {
+    speed: 500,
+    afterComplete: async () => {
+      renderMathInElement(document.body);
+    },
+  }).go();
+}
+
+Typing_Code();
+Typing();
